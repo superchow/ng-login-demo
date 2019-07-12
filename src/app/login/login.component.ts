db4604f2ba282334bc6cf482229617e2dee98613
周卫忠 <2665752129@qq.com>
2019-07-12 10:29:19 +0800

    1. tslint 优化代码格式;  2. 更改登录按钮 disabled 的判断方式  3. 添加全局监听后的销毁

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { EventManager } from '@angular/platform-browser';
 
 @Component({
@@ -8,10 +8,12 @@ import { EventManager } from '@angular/platform-browser';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
-export class LoginComponent implements OnInit {
-  validateForm: FormGroup;
-  public isDisable: Boolean = true;
-  public isLoading: Boolean = false;
+export class LoginComponent implements OnInit, OnDestroy {
+  private validateForm: FormGroup;
+  private attEventFn: any; // 如何正确定义类型
+  // public isDisable: boolean = true;
+  public isLoading: boolean = false;
+
   constructor(
     public router: Router,
     private fb: FormBuilder,
@@ -19,48 +21,60 @@ export class LoginComponent implements OnInit {
   ) { }
 
   submitForm(): void {
+    // Object.keys(this.validateForm.controls).forEach(k => {
+    //   this.validateForm.controls[k].markAsDirty();
+    //   this.validateForm.controls[k].updateValueAndValidity();
+    // });
+    // /* tslint:disable */
+    // for (const i in this.validateForm.controls) {
+    //   this.validateForm.controls[i].markAsDirty();
+    //   this.validateForm.controls[i].updateValueAndValidity();
+    // }
     for (const i in this.validateForm.controls) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
+      if (this.validateForm.controls.hasOwnProperty(i) ) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
     }
-    console.log(this.validateForm)
-    if(this.validateForm.valid){
+    console.log(this.validateForm.valid);
+    if (this.validateForm.valid) {
       this.isLoading = true;
       setTimeout(() => {
         sessionStorage.setItem('loginInfo', JSON.stringify(this.validateForm.value));
         this.isLoading = false;
-        this.router.navigate([''])
+        this.router.navigate(['']);
       }, 500);
     }
   }
 
-  validataFn (reg: RegExp) {
+  validataFn(reg: RegExp): (control: FormControl) => { [x: string]: boolean } | {} {
     return (control: FormControl) => {
-      if(!reg.test(control.value)){
-        return { unexpect: true }
+      if (!reg.test(control.value)) {
+        return { unexpect: true };
       }
-      return {}
-    }
+      return {};
+    };
   }
-  
+
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       userName: [null, [Validators.required, this.validataFn(/^[0-9]{4,8}$/)]],
       password: [null, [Validators.required, this.validataFn(/^[0-9a-zA-Z]{6,8}$/)]],
     });
-    this.validateForm.valueChanges.subscribe({
-      next: x => {
-        if(!!x.userName || !!x.password) {console.log(this.validateForm); this.isDisable = false}
-      }
-    });
+    // this.validateForm.valueChanges.subscribe({
+    //   next: x => {
+    //     if (!!x.userName || !!x.password) { this.isDisable = false; }
+    //   }
+    // });
     this.validateForm.patchValue({});
-    console.log(this.validateForm)
-    this.eventManager.addGlobalEventListener('window', 'keyup.enter', ()=>{
-      this.submitForm()
-    })
+    console.log(this.validateForm);
+    this.attEventFn = this.eventManager.addGlobalEventListener('window', 'keyup.enter', () => {
+      this.submitForm();
+    });
   }
-  ngOnDestroy(): void {
 
+  ngOnDestroy(): void {
+    this.attEventFn();
   }
 
 }
